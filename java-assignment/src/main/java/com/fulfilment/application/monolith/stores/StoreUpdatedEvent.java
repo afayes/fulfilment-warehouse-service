1c7d9d942133204be2894405ddb3529eae117632
afayes <afayes@hotmail.co.uk>
2026-02-17 22:32:05 +0000

    Implementation for task 2. Store (Must have) - Changes to StoreResource

---
+++ java-assignment/src/main/java/com/fulfilment/application/monolith/stores/StoreUpdatedEvent.java
@@ -0,0 +1,14 @@
+package com.fulfilment.application.monolith.stores;
+
+public class StoreUpdatedEvent {
+
+  private final Store store;
+
+  public StoreUpdatedEvent(Store store) {
+    this.store = store;
+  }
+
+  public Store getStore() {
+    return store;
+  }
+}
