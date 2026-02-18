4a231e11f4e04ed5416a457aa430ceddaac8a5ee
afayes <afayes@hotmail.co.uk>
2026-02-18 07:47:12 +0000

    WarehouseEndpointIT - added some new high level tests

--- java-assignment/src/test/java/com/fulfilment/application/monolith/warehouses/adapters/restapi/WarehouseEndpointIT.java
+++ java-assignment/src/test/java/com/fulfilment/application/monolith/warehouses/adapters/restapi/WarehouseEndpointIT.java
@@ -2,9 +2,11 @@ package com.fulfilment.application.monolith.warehouses.adapters.restapi;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 @QuarkusIntegrationTest
@@ -52,5 +54,64 @@ public class WarehouseEndpointIT {
             not(containsString("ZWOLLE-001")),
             containsString("AMSTERDAM-001"),
             containsString("TILBURG-001"));
+  }
+
+  @Test
+  public void testSimpleCreatingAndRetrievingWarehouse() {
+
+    final String path = "warehouse";
+
+    given()
+        .contentType(ContentType.JSON)
+        .body(
+            "{\"businessUnitCode\": \"IT.CREATE\", \"location\": \"AMSTERDAM-002\", \"capacity\": 20, \"stock\": 5}")
+        .post(path)
+        .then()
+        .statusCode(200)
+        .body("businessUnitCode", is("IT.CREATE"))
+        .body("location", is("AMSTERDAM-002"));
+
+    given()
+        .when()
+        .get(path + "/IT.CREATE")
+        .then()
+        .statusCode(200)
+        .body("businessUnitCode", is("IT.CREATE"))
+        .body("capacity", is(20))
+        .body("stock", is(5));
+  }
+
+  @Test
+  public void testSimpleReplacingWarehouse() {
+
+    final String path = "warehouse";
+
+    given()
+        .contentType(ContentType.JSON)
+        .body(
+            "{\"businessUnitCode\": \"IT.REPLACE\", \"location\": \"AMSTERDAM-002\", \"capacity\": 20, \"stock\": 5}")
+        .post(path)
+        .then()
+        .statusCode(200);
+
+    given()
+        .contentType(ContentType.JSON)
+        .body(
+            "{\"businessUnitCode\": \"IT.REPLACE\", \"location\": \"EINDHOVEN-001\", \"capacity\": 30, \"stock\": 5}")
+        .post(path + "/IT.REPLACE/replacement")
+        .then()
+        .statusCode(200)
+        .body("businessUnitCode", is("IT.REPLACE"))
+        .body("location", is("EINDHOVEN-001"))
+        .body("capacity", is(30));
+
+    given()
+        .when()
+        .get(path + "/IT.REPLACE")
+        .then()
+        .statusCode(200)
+        .body("location", is("EINDHOVEN-001"))
+        .body("capacity", is(30))
+        .body("stock", is(5));
   }
 }
